57bff8a5511e40e4afc38a4c1158d9e2d21aeeb8
virtusmaior <aliud@posteo.at>
2018-03-11 14:04:29 +0100

    Resolved Map bug

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux-immutable';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 import moment from 'moment';
 import _ from 'lodash';
 import {
